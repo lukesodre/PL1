82c083407ea028d39c616a2ab2ec9c6fae6d2669
Lucas Sodre <lucasasodre@gmail.com>
2014-09-20 16:49:57 -0400

    funcao1 added ruim

--- funcao1.cpp
+++ funcao1.cpp
@@ -2,51 +2,62 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <iostream>
 typedef const char* V;
 typedef std::vector<V> Record;
+typedef std::vector<Record> Page;
 
-#define BYTE 8
-
+#define REG_SIZE 10
 
 int fixed_len_sizeof(Record *record){ 
-        return  (sizeof(V) * record->size()) / BYTE;
+        return  (REG_SIZE * record->size());
 }
 
-
-void fixed_len_write(Record *record, unsigned char *buf){    
-    unsigned const char *tmp;
-    for(int i = 0; i < record->size() ; i++){
-//                tmp = (unsigned const char*) (*record)[i];
-//                buf[i]  = (unsigned char)tmp;
-
-        strcpy((char*) buf,(*record)[0]);
+void fixed_len_write(Record *record, void *buf) {
+    char *buf_return = new char[fixed_len_sizeof(record)];
+    for(Record::iterator i = record->begin(); i != record->end(); i++) {
+        //printf("%s -- sizeof: %ld\n", *i, sizeof(**i));
+        memcpy(buf, *i, REG_SIZE);
+        //printf("%s -- sizeof: %ld\n", (unsigned char *) buf, sizeof(buf));
+        //memcpy(buf_return, (char *) buf, REG_SIZE);
+        //printf("%s -- sizeof: %ld\n", buf_return, sizeof(buf_return));
+        buf = (char *) buf + REG_SIZE;
+        //printf("%s -- sizeof: %ld\n", (char *) buf-10, sizeof(buf));
+        //printf("times");
     }
-    printf("%s", buf); //(*record)[0]);
+    
+}
 
+void fixed_len_read(void *buf, int size, Record *record) {
+    assert(size == fixed_len_sizeof(record));
+    for(int i = 0 ; i < REG_SIZE; i++) {
+        memcpy((char *) record->at(i), (char *) buf + record->size()*i,REG_SIZE);
+    }
+    
 }
 
 int main() {
     
-    Record list;
-    V a = (V) 'a';
-    V b = (V) 'b';
+    Page *page = new Page();
+    Record *record1 = new Record();
+    Record *record2 = new Record();
     
-    list.push_back(a);
-    list.push_back(b);
-//     void *uf;
-    unsigned char *buf = (unsigned char*) malloc(sizeof(unsigned char)*fixed_len_sizeof(&list));
+    record1->push_back((V) "111222\0");
+    record2->push_back((V) "333444\0");
+    page->push_back(*record1);
+    page->push_back(*record2);
     
-    fixed_len_write(&list, buf);
-                    
-//     printf(" %c %d",list.at(0), fixed_len_sizeof(&list));
+    long int page_size = 0;    
+    for(Page::iterator i = page->begin(); i != page->end(); i++) {
+        //printf("Record data: %c\n", (const char *) *(*i));
+        page_size += fixed_len_sizeof(*(*i));
+    }
+    printf("Page size: %ld", page_size);
+    
+    char *buf = new char[page_size];
+    
+    //fixed_len_write(record, buf);
+    //fixed_len_read(buf,REG_SIZE,record);               
     return 0;
 }
-
-/*
-        V byte[BYTE];
-        for(int i = 0 ; i < record.size() ; i++)
-            for(int j = (BYTE - 1) ; j >=0 ; j--){
-                *buf = (record[i] & (1 << j)) ? '1' : '0';
-                 buf++;
-            }*/
